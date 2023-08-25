4dfa948362410cf67bd1bd61d12f3114d02b7d8d
lucas <lucas.carvalhu.ferreira@gmail.com>
2023-08-25 00:39:55 -0300

    Adiciona lista de pokemons de forma dinâmica

--- REAME.md
+++ REAME.md
@@ -1,1 +1,4 @@
 Projeto iniciado no bootcamp Santander 2023.
+
+Link github
+https://github.com/lucascfa/projeto_pokedex.git
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,35 @@ const OFFSET = 0;
 const LIMIT = 10;
 const URL = `https://pokeapi.co/api/v2/pokemon/?offset=${OFFSET}&limit=${LIMIT}`;
 
+function convertPokemonToLi(pokemon){
+    return `
+    <li class="pokemon">
+    <span class="number">#001</span>
+     <span class="name">${pokemon.name}</span>
+
+     <div class="detail">
+        <ol class="types">
+            <li class="type">Grass</li>
+            <li class="type">Poison</li>
+        </ol>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/11.svg" alt="${pokemon.name}">
+    </div>
+</li>
+    `
+}
+
+const pokemonLista = document.getElementById('listaPokemon');
+
+
 fetch(URL)
 .then((response) => response.json())
-.then((responseBody) => console.log(responseBody))
+.then((responseBody) => responseBody.results)
+.then((pokemons) => {
+
+    for(let i = 0; i < pokemons.length; i++){
+        const pokemon = pokemons[i]
+        pokemonLista.innerHTML += convertPokemonToLi(pokemon);
+    }
+})
 .catch((error) => console.log(error));
+
